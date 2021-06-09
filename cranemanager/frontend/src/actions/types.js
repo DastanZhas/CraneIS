1870f6b3dd8fe6dc4f3978ddfaf32d65a99d8398
DastanZhas <dastanzhas01@gmail.com>
2021-06-09 13:46:41 +0600

    Auth State

--- cranemanager/frontend/src/actions/types.js
+++ cranemanager/frontend/src/actions/types.js
@@ -3,3 +3,6 @@ export const DELETE_CRANE = "DELETE_CRANE";
 export const ADD_CRANE = "ADD_CRANE";
 export const GET_ERRORS = "GET_ERRORS";
 export const CREATE_MESSAGE = "CREATE_MESSAGE";
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
--- cranemanager/frontend/src/reducers/index.js
+++ cranemanager/frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import cranes from './cranes';
 import errors from './errors';
 import messages from './messages';
+import auth from './auth';
 
 export default combineReducers({
     cranes,
     errors,
-    messages
+    messages,
+    auth
 });
